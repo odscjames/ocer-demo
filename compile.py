693a3a8c8e802f36a2df3ebd10db5a6715e840e8
James Baster <james.baster@opendataservices.coop>
2018-05-11 15:21:01 +0100

    Use slug not filename as that's the one that was being used before

--- compile.py
+++ compile.py
@@ -7,13 +7,17 @@ for directory in glob.glob("/open-contracting-extension-registry/*"):
     if os.path.isdir(directory):
 
         id = directory.split('/')[-1]
-        print(id)
+        print("Input: " + id)
 
         entry_json_file = os.path.join(directory, "entry.json")
-        out_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extension_registry", "extensions", id + ".json")
 
         with open(entry_json_file) as fp:
             entry_obj = json.load(fp)
+
+            out_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "extension_registry", "extensions",
+                                    entry_obj[0]['slug'] + ".json")
+
+            print("Makes output: " + entry_obj[0]['slug'])
 
             url = entry_obj[0]['url']
             if url[-21:] != 'master/extension.json':
@@ -23,6 +27,7 @@ for directory in glob.glob("/open-contracting-extension-registry/*"):
             if url[:33] != 'https://raw.githubusercontent.com':
                 raise Exception(url[:33])
             url = 'https://github.com' + url[33:]
+
 
             out_data = {
                 "category":entry_obj[0]['category'],
