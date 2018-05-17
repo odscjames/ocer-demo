979deced071f77aa81dab8beeadf8482f90c85e0
James Baster <james.baster@opendataservices.coop>
2018-05-17 10:10:34 +0100

    Load Data

--- extension_registry/compile.py
+++ extension_registry/compile.py
@@ -4,4 +4,4 @@ import os
 
 if __name__ == "__main__":
     ocdsextensionregistry.compile.registry_csv_filename = os.path.dirname(__file__) + '/extensions.csv'
-    ocdsextensionregistry.compile.compile()
+    ocdsextensionregistry.compile.compile_registry()
--- extension_registry/ocdsextensionregistry/compile.py
+++ extension_registry/ocdsextensionregistry/compile.py
@@ -1,7 +1,35 @@
+import csv
+from .models import ExtensionModel
+from .util import string_to_boolean
+
 
 # Options that are set by external callers
 registry_csv_filename = None
 
+# Variables for tracking state
+extensions = {}
+
+
 # Methods that external callers can use
-def compile():
-    print(registry_csv_filename)
+def compile_registry():
+    _load_data()
+
+
+def _load_data():
+    with open(registry_csv_filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        reader.__next__()  # Throw away the heading line
+        for row in reader:
+            # To decide if row has any data in, check it has values and it has an id.
+            if len(row) > 0:
+                extension_id = row[0].lower().strip()
+                if extension_id:
+                    if extension_id in extensions.keys():
+                        raise Exception("Extension %s is already registered! (Duplicate is on line %d)" % (
+                        extension_id, reader.line_num))
+                    extension_model = ExtensionModel(
+                        repository_url=row[1],
+                        category=row[2],
+                        core=string_to_boolean(row[3])
+                    )
+                    extensions[extension_id] = extension_model
